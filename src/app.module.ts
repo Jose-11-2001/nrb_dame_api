341f642f64f6a24f82b284f9f56075fae8ec3007
Jose-11-2001 <josephmbukwa2001@gmail.com>
2026-05-13 14:41:07 +0200

    updating21

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,6 @@
-// src/app.module.ts
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule, ConfigService } from '@nestjs/config';
-import { typeOrmConfig } from './database/typeorm.config';
 import { NationalIdModule } from './nationa_id/national_id.module';
 import { DeathModule } from './death/death.module';
 
@@ -16,9 +14,9 @@ import { DeathModule } from './death/death.module';
       useFactory: async (configService: ConfigService) => ({
         type: 'postgres',
         host: configService.get('DB_HOST', 'localhost'),
-        port: configService.get('DB_PORT', 5432),
+        port: configService.get<number>('DB_PORT', 5432),
         username: configService.get('DB_USERNAME', 'postgres'),
-        password: configService.get('DB_PASSWORD', '2001'),
+        password: configService.get('DB_PASSWORD', '2001'), // Your actual password
         database: configService.get('DB_DATABASE', 'nrbdb'),
         entities: [__dirname + '/**/*.entity{.ts,.js}'],
         synchronize: configService.get('DB_SYNC', true),
